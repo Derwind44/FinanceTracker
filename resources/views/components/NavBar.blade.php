1f3fc8a727ceb3919f994df62f399d71b5b170ae
Derwind44 <derwind44@gmail.com>
2024-07-07 19:58:30 +0700

    Nav

---
+++ resources/views/components/NavBar.blade.php
@@ -0,0 +1,9 @@
+<div class="d-inline-flex bg-dark border rounded-3 mx-5 mt-5">
+    <nav class="nav nav-pills d-inline-flex justify-content-start ">
+        <a class="flex-sm text-sm-center nav-link active mr-2" href="#">Dashboard</a>
+        <a class="flex-sm text-sm-center nav-link active mx-2" href="#">Expense</a>
+        <a class="flex-sm text-sm-center nav-link active ml-2" href="#">Report</a>
+    </nav>
+</div>
+
+
